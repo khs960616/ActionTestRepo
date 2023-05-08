9c9249d61ab40747beaeca96746f85d870c16ba5
anjfkrhdkdle@naver.com <khs960616>
2023-05-08 21:59:10 +0900

    add test controller

---
+++ src/main/java/my/khss/action/controller/HelloworldController.java
@@ -0,0 +1,13 @@
+package my.khss.action.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloworldController {
+
+    @GetMapping("/hello")
+    public String helloGitAction() {
+        return "Hello";
+    }
+}
